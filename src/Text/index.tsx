7e873bf752d96759fdaf42f47b77a1655c6c1ae7
Mukul Jain <jainmukul1996@gmail.com>
2022-02-27 00:43:29 +0530

    chore: add styled-component

--- src/Text/index.tsx
+++ src/Text/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const weightConfig = {
   bold: 700,
@@ -19,25 +19,26 @@ type IProps = {
   weight?: keyof typeof weightConfig;
   truncated?: boolean;
   size?: keyof typeof sizeConfig;
+  className?: string;
 };
 const StyledText = styled.p<IProps>`
   margin: 0;
   ${({ weight, size }) => {
     return `
-    font-weight: ${weightConfig[weight || 'regular']};
-    font-size: ${sizeConfig[size || 'rg']}px;
+    font-weight: ${weightConfig[weight || "regular"]};
+    font-size: ${sizeConfig[size || "rg"]}px;
     `;
   }}
 `;
 
-export type TextProps = React.ComponentPropsWithoutRef<'p'> & IProps;
+export type TextProps = React.ComponentPropsWithoutRef<"p"> & IProps;
 
 const Text = ({ weight, size, truncated, className, ...rest }: TextProps) => {
   return (
     <StyledText
       weight={weight}
       size={size}
-      className={`${className} ${truncated ? 'truncate' : ''}`}
+      className={`${className} ${truncated ? "truncate" : ""}`}
       {...rest}
     />
   );
