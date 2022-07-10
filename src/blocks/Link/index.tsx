4dab10f7f9fcebc466adc2810a8ad368e38178a5
Mukul Jain <jainmukul1996@gmail.com>
2022-07-10 21:39:31 +0530

    fix(link): make it simple

--- src/blocks/Link/index.tsx
+++ src/blocks/Link/index.tsx
@@ -1,58 +1,17 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import Paper from '../../blocks/Paper';
 
-const Wrapper = styled(Paper).attrs({
-  'data-component': 'Link',
-})<{ active?: boolean }>`
+const Wrapper = styled.a<{ active?: boolean }>`
   display: inline-flex;
   color: ${({ theme }) => theme.blocks.link.color};
   text-decoration: none;
   cursor: pointer;
-  ${({ theme, active }) => active && `color: ${theme.blocks.link.color}`};
 `;
 
-export interface LinkProps extends React.ComponentPropsWithoutRef<'div'> {
-  icon?: React.ReactNode;
-  title: string;
-  href?: string;
-  id: string;
-  action?: (
-    event: React.SyntheticEvent,
-    data: { id: string; index?: number },
-  ) => void;
-  index?: number;
-  active?: boolean;
-}
+export type LinkProps = React.ComponentPropsWithRef<'a'>;
 
-const Link = ({
-  href,
-  title,
-  icon,
-  id,
-  action,
-  index,
-  active,
-  ...rest
-}: LinkProps) => {
-  return (
-    <Wrapper
-      active={active}
-      flex
-      alignItems="center"
-      gutter={12}
-      href={href}
-      as={href ? 'a' : 'div'}
-      onClick={(event: React.MouseEvent) => {
-        action &&
-          action(event, { id, index: index === undefined ? -1 : index });
-      }}
-      {...rest}
-    >
-      {icon}
-      <span>{title}</span>
-    </Wrapper>
-  );
+const Link = (props: LinkProps) => {
+  return <Wrapper {...props}></Wrapper>;
 };
 
 export default Link;
